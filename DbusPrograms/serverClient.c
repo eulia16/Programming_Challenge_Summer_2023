6f38495fdc053a1d3be7cc70c87bb5416db95171
EUli16 <ethan.uliano@gmail.com>
2023-07-11 11:56:11 -0400

    simple server client dbus program, client sends specifications of how it will receive signals, server sends signal, client receives signal, basic outline of how data transfer will work between squegg and pure data will work

---
+++ DbusPrograms/serverClient.c
@@ -0,0 +1,186 @@
+#include <dbus/dbus.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Connect to the DBUS bus and send a broadcast signal
+ */
+void sendsignal(char* sigvalue)
+{
+   DBusMessage* msg;
+   DBusMessageIter args;
+   
+   
+   
+   dbus_uint32_t serial = 0;
+   
+    DBusConnection* conn;
+   DBusError err;
+   int ret;
+   
+   
+   dbus_error_init(&err);
+
+   // connect to the DBUS system bus, and check for errors
+   conn = dbus_bus_get(DBUS_BUS_SYSTEM, &err);
+   if (dbus_error_is_set(&err)) { 
+      fprintf(stderr, "Connection Error (%s)\n", err.message); 
+      dbus_error_free(&err); 
+   }
+   if (NULL == conn) { 
+      exit(1); 
+   }
+
+   // register our name on the bus, and check for errors
+   ret = dbus_bus_request_name(conn, "test.signal.source", DBUS_NAME_FLAG_REPLACE_EXISTING , &err);
+   if (dbus_error_is_set(&err)) { 
+      fprintf(stderr, "Name Error (%s)\n", err.message); 
+      dbus_error_free(&err); 
+   }
+   if (DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER != ret) { 
+      exit(1);
+   }
+   
+
+   printf("Sending signal with value %s\n", sigvalue);
+
+   // initialise the error value
+   
+
+   // create a signal & check for errors 
+   msg = dbus_message_new_signal("/test/signal/Object", // object name of the signal
+                                 "test.signal.Type", // interface name of the signal
+                                 "Test"); // name of the signal
+   if (NULL == msg) 
+   { 
+      fprintf(stderr, "Message Null\n"); 
+      exit(1); 
+   }
+
+   // append arguments onto signal
+   dbus_message_iter_init_append(msg, &args);
+   if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &sigvalue)) {
+      fprintf(stderr, "Out Of Memory!\n"); 
+      exit(1);
+   }
+
+   // send the message and flush the connection
+   if (!dbus_connection_send(conn, msg, &serial)) {
+      fprintf(stderr, "Out Of Memory!\n"); 
+      exit(1);
+   }
+   dbus_connection_flush(conn);
+   
+   printf("Signal Sent\n");
+   
+   // free the message and close the connection
+   dbus_message_unref(msg);
+     //dbus_connection_close(conn);
+}
+
+
+
+
+/**
+ * Listens for signals on the bus
+ */
+void receive()
+{
+   DBusMessage* msg;
+   DBusMessageIter args;
+   DBusConnection* conn;
+   DBusError err;
+   int ret;
+   char* sigvalue;
+
+   printf("Listening for signals\n");
+
+   // initialise the errors
+   dbus_error_init(&err);
+   
+   // connect to the bus and check for errors
+   conn = dbus_bus_get(DBUS_BUS_SYSTEM, &err);
+   if (dbus_error_is_set(&err)) { 
+      fprintf(stderr, "Connection Error (%s)\n", err.message);
+      dbus_error_free(&err); 
+   }
+   if (NULL == conn) { 
+      exit(1);
+   }
+   
+   // request our name on the bus and check for errors
+   ret = dbus_bus_request_name(conn, "test.signal.sink", DBUS_NAME_FLAG_REPLACE_EXISTING , &err);
+   if (dbus_error_is_set(&err)) { 
+      fprintf(stderr, "Name Error (%s)\n", err.message);
+      dbus_error_free(&err); 
+   }
+   if (DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER != ret) {
+      exit(1);
+   }
+
+   // add a rule for which messages we want to see
+   dbus_bus_add_match(conn, "type='signal',interface='test.signal.Type'", &err); // see signals from the given interface
+   dbus_connection_flush(conn);
+   if (dbus_error_is_set(&err)) { 
+      fprintf(stderr, "Match Error (%s)\n", err.message);
+      exit(1); 
+   }
+   printf("Match rule sent\n");
+
+   // loop listening for signals being emmitted
+   while (true) {
+
+      // non blocking read of the next available message
+      dbus_connection_read_write(conn, 0);
+      msg = dbus_connection_pop_message(conn);
+
+      // loop again if we haven't read a message
+      if (NULL == msg) { 
+         sleep(1);
+         continue;
+      }
+
+      // check if the message is a signal from the correct interface and with the correct name
+      if (dbus_message_is_signal(msg, "test.signal.Type", "Test")) {
+         
+         // read the parameters
+         if (!dbus_message_iter_init(msg, &args))
+            fprintf(stderr, "Message Has No Parameters\n");
+         else if (DBUS_TYPE_STRING != dbus_message_iter_get_arg_type(&args)) 
+            fprintf(stderr, "Argument is not string!\n"); 
+         else
+            dbus_message_iter_get_basic(&args, &sigvalue);
+         
+         printf("Got Signal with value %s\n", sigvalue);
+      }
+
+      // free the message
+      dbus_message_unref(msg);
+   }
+   // close the connection
+   dbus_connection_close(conn);
+}
+
+int main(int argc, char** argv)
+{
+
+  
+   
+   if (2 > argc) {
+      printf ("Enter proper number of args\n");
+      return 1;
+   }
+   char* param = "no param";
+   if (3 >= argc && NULL != argv[2]) param = argv[2];
+   if (0 == strcmp(argv[1], "send"))
+      sendsignal(param);
+   else if (0 == strcmp(argv[1], "receive"))
+      receive();
+   else {
+      printf ("something went wrong\n");
+      return 1;
+   }
+   return 0;
+}
